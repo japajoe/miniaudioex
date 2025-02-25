cdaf25240dc48820d18ef0b842bb4344d11f4564
Wesley <wjapajoe@ziggo.nl>
2025-02-25 13:37:32 -0300

    Test build for windows

--- include/miniaudio.h
+++ include/miniaudio.h
@@ -247,8 +247,6 @@ typedef ma_uint16 wchar_t;
     #define MA_NO_INLINE
 #endif
 
-#define MA_DLL
-
 /* MA_DLL is not officially supported. You're on your own if you want to use this. */
 #if defined(MA_DLL)
     #if defined(_WIN32)
